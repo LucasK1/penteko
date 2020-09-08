09fbba94d9c4b13c4ae4750eff889345b83ab8ea
Łukasz Kozłowski <ryuke12@gmail.com>
2020-09-08 17:13:16 +0200

    Fix the menu, menu disappears when link clicked

--- main.js
+++ main.js
@@ -2,15 +2,27 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-items');
 const navItems = document.querySelectorAll('.nav-item');
 
-burger.addEventListener('click', showNav);
+burger.addEventListener('click', toggleNav);
+navItems.forEach((item) => item.addEventListener('click', toggleNav));
 
-function showNav() {
+function toggleNav() {
   nav.classList.toggle('nav-active');
   animateLinks();
+  animateBurger();
 }
 
 function animateLinks() {
   navItems.forEach((item, index) => {
-    item.style.animation = `navItemFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+    if (item.style.animation) {
+      item.style.animation = '';
+    } else {
+      item.style.animation = `navItemFade 0.5s ease forwards ${
+        index / 7 + 0.5
+      }s`;
+    }
   });
+}
+
+function animateBurger() {
+  burger.classList.toggle('toggle');
 }
