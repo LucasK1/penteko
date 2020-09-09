b60499717bdf6f006f13c7bc698db7d6bddbcacd
Łukasz Kozłowski <ryuke12@gmail.com>
2020-09-09 12:44:31 +0200

    Add projects part, modal with more projects

--- main.js
+++ main.js
@@ -2,10 +2,26 @@ const burger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar__items');
 const navbarItems = document.querySelectorAll('.navbar__item');
 const logo = document.querySelector('.navbar__logo');
+const moreBtn = document.querySelector('.projects__btn-more');
+const modal = document.querySelector('.projects__modal');
 
+// clicking the burger opens the drawer menu
 burger.addEventListener('click', toggleNav);
+
+// Clicking a navlink closes the modal
 if (screen.width >= '768px') {
   navbarItems.forEach((item) => item.addEventListener('click', toggleNav));
+}
+// More btn in projects section opens a modal with all projects
+moreBtn.addEventListener('click', toggleModal);
+modal.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  if (modal.style.display === '') {
+  modal.style.display = 'block';
+  } else {
+    modal.style.display = '';
+  }
 }
 
 function toggleNav() {
